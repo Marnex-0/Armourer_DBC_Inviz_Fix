ac2d3271870bbb1ef0bd1e015724fb1ef745d1c5
Marnex <marnex@tutanota.com>
2026-01-30 14:41:08 +0200

    Build works in DEV enviroment and launches in normal but does not function in normal

---
+++ src/main/java/com/marnex/armorfix/ArmorFixLateMixinLoader.java
@@ -0,0 +1,25 @@
+package com.marnex.armorfix;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
+import com.gtnewhorizon.gtnhmixins.LateMixin;
+
+@LateMixin
+public class ArmorFixLateMixinLoader implements ILateMixinLoader {
+
+    @Override
+    public String getMixinConfig() {
+        return "mixins.armorfix.late.json";
+    }
+
+    @Override
+    public List<String> getMixins(Set<String> loadedMods) {
+        List<String> mixins = new ArrayList<>();
+        mixins.add("JBRAdisableInvisCheck");
+
+        return mixins;
+    }
+}
---
+++ src/main/java/com/marnex/armorfix/mixins/JBRAdisableInvisCheck.java
@@ -0,0 +1,21 @@
+package com.marnex.armorfix.mixins;
+
+import net.minecraft.client.entity.AbstractClientPlayer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import JinRyuu.JBRA.RenderPlayerJBRA;
+
+@Mixin(value = RenderPlayerJBRA.class, remap = false)
+public class JBRAdisableInvisCheck {
+
+    @Redirect(
+        method = "preRenderCallback(Lnet/minecraft/client/entity/AbstractClientPlayer;F)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/AbstractClientPlayer;isInvisible()Z"))
+    private boolean alwaysVisible(AbstractClientPlayer player) {
+        System.out.println("Hey I'm HERE DUMBASS!!!");
+        return false;
+    }
+}
