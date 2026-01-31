43b0e89dc9db912d7c8a79eb09d97b3076b755e3
Marnex <marnex@tutanota.com>
2026-02-01 01:10:59 +0200

    There needed to be a Mod class for late mixins to take effect, works in prod now.

--- src/main/java/com/marnex/armorfix/ArmorFix.java
+++ src/main/java/com/marnex/armorfix/ArmorFix.java
@@ -7,7 +7,5 @@ import cpw.mods.fml.common.Mod;
 
 @Mod(modid = ArmorFix.MODID, version = "1.0", name = "ArmorFix", acceptedMinecraftVersions = "[1.7.10]")
 public class ArmorFix {
-
     public static final String MODID = "armorfix";
-    public static final Logger LOG = LogManager.getLogger(MODID);
 }
